3d8b7d97cf3397a9ec4ec1e6d91933a22228bedf
Kevin <515867748@qq.com>
2016-04-08 10:11:16 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 左坤 on 16/4/8.
+//  Copyright © 2016年 Mr.Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by 左坤 on 16/4/8.
+//  Copyright © 2016年 Mr.Kevin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
